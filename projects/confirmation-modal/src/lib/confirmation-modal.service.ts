2d6bc342c3507c4183de9cf17c9d459f444086f2
Isac Petinate <ipetinate@viceri.com.br>
2020-03-04 11:32:16 -0300

    Commit

--- projects/confirmation-modal/src/lib/confirmation-modal.service.ts
+++ projects/confirmation-modal/src/lib/confirmation-modal.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material';
-import { Observable } from 'rxjs';
 import { ConfirmationModal } from './confirmation-model';
 import { ConfirmationModalComponent } from './confirmation-modal.component';
 
